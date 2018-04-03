3345e13dab051010ceccd177ad8729657a327c73
montoymi <miguel.montoya.garcia@gmail.com>
2018-04-02 20:21:47 -0500

    Se actualiza la clase test

--- src/main/java/com/amadeus/betgroup/rest/account/UserResource.java
+++ src/main/java/com/amadeus/betgroup/rest/account/UserResource.java
@@ -8,19 +8,25 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.Response.Status.CREATED;
 import static javax.ws.rs.core.Response.Status.OK;
 
 @Path("/users")
-@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_JSON})
 public class UserResource {
     private UserService userService = new UserService();
 
-
     @GET
     public Response validateLogin(@QueryParam("username") String username, @QueryParam("password") String password) {
-        User user = userService.validateLogin( username, password);
+        User user = userService.validateLogin(username, password);
         return Response.status(OK).entity(user).build();
+    }
+
+    @POST
+    public Response registraUsuario(User user) {
+        userService.registraUsuario(user);
+        return Response.status(CREATED).entity(user).build();
     }
 }
 
